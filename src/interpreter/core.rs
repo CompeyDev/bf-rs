b54e8741b9a7fb4fbdc62d83d51bc0d69669541e
Compey <hi@devcomp.xyz>
2023-05-01 16:12:25 +0530

    refactor: cleaner instruction & lexer implementation

--- src/interpreter/core.rs
+++ src/interpreter/core.rs
@@ -1,15 +1,26 @@
 use crate::utils;
+use utils::AnyMap as HashMap;
 
 #[derive(Debug, Clone)]
 pub struct BrainfuckInstance {
     vm: Vec<usize>,
-    current_ptr: usize,
-    next_ptr: usize,
+    current_ptr: u32,
+    next_ptr: u32,
     in_loop: bool,
 }
 
-pub struct Lexer {
-    instance: BrainfuckInstance,
+pub struct Lexer;
+
+pub struct Instructions {
+    instructions: HashMap<Instruction>,
+}
+
+#[derive(Hash, Eq, PartialEq)]
+pub enum Instruction {
+    MovR,
+    MovL,
+    Incr,
+    Decr,
 }
 
 impl BrainfuckInstance {
@@ -31,62 +42,108 @@ impl BrainfuckInstance {
             let instr_string = instr_char.to_string();
             let instr = instr_string.as_str();
 
-            if let None = self.vm.get(self.current_ptr) {
-                self.vm.insert(self.current_ptr, 0);
+            if let None = self.vm.get(self.current_ptr as usize) {
+                self.vm.insert(self.current_ptr as usize, 0);
             }
 
-            let mut lexer_instance = Lexer::new((&*self).to_owned());
+            let mut lexer_instance = Lexer::new();
 
-            let mutated_bf_instance = lexer_instance.parse(instr, instr_pos, in_loop);
+            let cloned_instance = &mut self.clone();
+            let (mutated_in_loop, mutated_bf_instance) = lexer_instance.parse(instr, instr_pos)(in_loop, cloned_instance);
 
-            self.vm = mutated_bf_instance.vm;
-            self.current_ptr = mutated_bf_instance.current_ptr;
-            self.next_ptr = mutated_bf_instance.current_ptr;
-            self.in_loop = mutated_bf_instance.in_loop;
+            let new_bf_instance = mutated_bf_instance.clone();
+
+            self.vm = new_bf_instance.vm;
+            self.current_ptr = new_bf_instance.current_ptr;
+            self.next_ptr = new_bf_instance.current_ptr;
+            self.in_loop = mutated_in_loop;
         }
         println!("vm: {:#?}", self.vm);
     }
 }
 
 impl Lexer {
-    pub fn new(instance: BrainfuckInstance) -> Self {
-        return Self { instance };
+    pub fn new() -> Self {
+        return Self {};
     }
 
-    pub fn parse(&mut self, instr: &str, instr_pos: usize, in_loop: bool) -> BrainfuckInstance {
-        match instr {
-            ">" => {
-                if !in_loop {
-                    self.instance.current_ptr += 1;
-                    self.instance.next_ptr += 1;
-                }
-            }
-
-            "<" => {
-                if !in_loop {
-                    self.instance.current_ptr -= 1;
-                    self.instance.next_ptr -= 1;
-                }
-            }
-
-            "+" => {
-                if !in_loop {
-                    self.instance.vm[self.instance.current_ptr] += 1;
-                }
-            }
-
-            "-" => {
-                if !in_loop {
-                    self.instance.vm[self.instance.current_ptr] -= 1;
-                }
-            }
-
+    pub fn parse(&mut self, instr: &str, instr_pos: usize) -> fn(bool, &mut BrainfuckInstance) -> (bool, &BrainfuckInstance) {
+        let instr_type = match instr {
+            ">" => Instruction::MovR,
+            "<" => Instruction::MovL,
+            "+" => Instruction::Incr,
+            "-" => Instruction::Decr,
             &_ => utils::throw_err(
                 "VM",
                 format!("invalid instruction `{}` at ptr `{}`", instr, instr_pos).as_str(),
             ),
-        }
+        };
 
-        return self.instance.clone();
+        let mut instructions = Instructions::new();
+
+        instructions.populate();
+
+        let instr_handler = instructions.get_handler(instr_type).unwrap();
+
+        return instr_handler.clone();
+    }
+}
+
+impl Instructions {
+    pub fn new() -> Self {
+        return Self {
+            instructions: HashMap::new(),
+        };
+    }
+
+    pub fn populate(&mut self) {
+        self.register(Instruction::MovR, |in_loop, instance| {
+            if !in_loop {
+                instance.current_ptr += 1;
+                instance.next_ptr += 1;
+            }
+
+            return (in_loop, &*instance)
+        });
+
+        self.register(Instruction::MovL, |in_loop, instance| {
+            if !in_loop && instance.current_ptr != 0 {
+                instance.current_ptr -= 1;
+                instance.next_ptr -= 1;
+            } else {
+                utils::throw_err("VM", "current pointer at 0 or in loop");
+            }
+
+            return (in_loop, &*instance)
+        });
+
+        self.register(Instruction::Incr, |in_loop, instance| {
+            if !in_loop {
+                instance.vm[instance.current_ptr as usize] += 1;
+            }
+
+            return (in_loop, &*instance)
+        });
+
+        self.register(Instruction::Decr, |in_loop, instance| {
+            let current_mem_chunk = instance.vm[instance.current_ptr as usize];
+            
+            if !in_loop && current_mem_chunk != 0 {
+                instance.vm[instance.current_ptr as usize] -= 1; // ERROR: ATTEMPT TO SUBTRACT WITH OVERFLOW
+            } else {
+                utils::throw_err("VM", "current pointer at 0 or in loop");
+            }
+
+            return (in_loop, &*instance)
+        });
+    }
+
+    pub fn register(&mut self, instr: Instruction, implementation: fn(bool, &mut BrainfuckInstance) -> (bool, &BrainfuckInstance)) {
+        self.instructions.insert(instr, implementation);
+
+    }
+
+    pub fn get_handler(&self, instr: Instruction) -> Option<&fn(bool, &mut BrainfuckInstance) -> (bool, &BrainfuckInstance)> {
+        return self.instructions.get::<fn(bool, &mut BrainfuckInstance) -> (bool, &BrainfuckInstance)>(instr);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+#![feature(box_into_inner)]
+
 mod interpreter;
 
 use std::{fs, process::exit};
 
+use clap::{CommandFactory, Parser};
 use interpreter::{core, utils};
-use clap::{Parser, CommandFactory};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -14,8 +16,7 @@ struct Args {
     #[arg(short, long)]
     code: Option<String>,
 
-    filename: Option<String>
-    
+    filename: Option<String>,
 }
 
 fn main() {
@@ -30,22 +31,20 @@ fn main() {
                 if let Err(_) = Args::command().print_help() {
                     utils::throw_err("CLI", "failed to display help menu")
                 }
-                
+
                 exit(0);
             }
-        }
+        },
     };
 
     let to_strip_illegal = match runtime_args.strip_illegal {
         Some(val) => val,
-        None => false
+        None => false,
     };
 
     if to_strip_illegal {
         input_code = utils::strip_code(input_code.as_str());
     }
-
-    
 
     core::BrainfuckInstance::new().load_string(input_code); // should be [ 0, 1, 1 ]
 }
