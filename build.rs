49a83e76e61ae742f928b02fd234d9000017a8d3
Compey <hi@devcomp.xyz>
2023-05-02 21:17:10 +0530

    chore: fixed build pipeline (2)

--- build.rs
+++ build.rs
@@ -1,8 +1,6 @@
-use windres::Build;
-
 fn main() {
     if cfg!(target_os = "windows") {
-        Build::new()
+        windres::Build::new()
             .compile("assets/prog_meta.rc")
             .unwrap();
     }
